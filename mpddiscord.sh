6ea35548a14e417abbb9dd496969b66c21261969
FearlessSwiftie <fearlessswiftie1322@gmail.com>
2017-04-02 15:48:15 -0700

    removed nohup and &

--- mpddiscord.sh
+++ mpddiscord.sh
@@ -2,7 +2,7 @@
 
 ### if you decide to move the script, i.e. to PATH, then edit
 ### /path/to/main.js as needed
-# nohup node /path/to/main.js &
+# node /path/to/main.js
 
-nohup node main.js &
+node main.js
 
