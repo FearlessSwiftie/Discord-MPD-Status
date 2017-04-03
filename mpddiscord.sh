8fd2bb6f06a7b43b9a52a58d4225fc372c25ae9d
FearlessSwiftie <fearlessswiftie1322@gmail.com>
2017-04-02 23:52:14 -0700

    update

--- mpddiscord.sh
+++ mpddiscord.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-### if you decide to move the script, i.e. to PATH, then edit
-### /path/to/main.js as needed
+### if you decide to move the script, i.e. to PATH, then edit /path/to/main.js as needed ###
 # node /path/to/main.js
 
 node main.js
